730bb87daf245e55de381c2a4087a85c6985d05a
FiruzzZ <romerojoseluiz@gmail.com>
2015-06-11 13:10:34 -0300

    retoques al new renderer combobox color

--- src/utilities/swing/components/ComboBoxColorRenderer.java
+++ src/utilities/swing/components/ComboBoxColorRenderer.java
@@ -8,7 +8,7 @@ import javax.swing.JList;
 
 /**
  * Permite especificar un foreground color para cada item mediante la implementación del método
- * {@link #getColorByItem(java.lang.Object)}.
+ * {@link #getColorByItem(int, java.lang.Object)}.
  *
  * @author FiruzzZ
  */
@@ -22,15 +22,19 @@ public abstract class ComboBoxColorRenderer extends DefaultListCellRenderer {
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
 
         JLabel c = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-        c.setForeground(getColorByItem(value));
+        if (index > -1) { //el renderer también es llamado aunque el combo esté vacío
+            c.setForeground(getColorByItem(index, value));
+        }
         return this;
     }
 
     /**
      * Es llamado por cada item contenido en el JList del combobox
+     * <i>Ojo! si el 1er item es el típico &lt Elegir&gt</i>
      *
+     * @param index
      * @param value item del combo
      * @return algún {@link Color} o null
      */
-    public abstract Color getColorByItem(Object value);
+    public abstract Color getColorByItem(int index, Object value);
 }
